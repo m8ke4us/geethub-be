125fa520c11915e7ba2441fe4c2277a26fb000b7
yuhyeminn <yuhyemin96@gmail.com>
2023-09-16 22:56:48 +0900

    geethub initial commit

---
+++ src/main/java/com/m8ke4us/geethub/GeethubApplication.java
@@ -0,0 +1,13 @@
+package com.m8ke4us.geethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeethubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeethubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m8ke4us/geethub/GeethubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m8ke4us.geethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeethubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
